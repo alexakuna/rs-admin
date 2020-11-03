b68cb149a44f9262411dddb43368838ae78f78f5
mac <mac@macs-MacBook-Air.local>
2020-11-03 15:57:50 +0200

    reinit 3

---
+++ public/dist/precache-manifest.1bb41971b1097300e8c4cd071a6942c1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e211b6f184ca7298dd",
+    "url": "/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "f7905217d79ff38d352a",
+    "url": "/css/chunk-868b1ebc.56ecfed1.css"
+  },
+  {
+    "revision": "f32e4c6ddbfdbbcafc7b",
+    "url": "/css/chunk-vendors.73e1f5b0.css"
+  },
+  {
+    "revision": "949c55feb329330f7a5e19f8459549d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e211b6f184ca7298dd",
+    "url": "/js/app.da3353ac.js"
+  },
+  {
+    "revision": "0834fee724ba4e017164",
+    "url": "/js/chunk-2d0a3153.3865f2c2.js"
+  },
+  {
+    "revision": "1c0d74bbab9ff0852c1c",
+    "url": "/js/chunk-2d0aa7b8.0a4f81ac.js"
+  },
+  {
+    "revision": "b846c744741e3c53634a",
+    "url": "/js/chunk-2d0c19d5.59a98083.js"
+  },
+  {
+    "revision": "664f071c1364d4a6de1f",
+    "url": "/js/chunk-2d0c7374.df2491bf.js"
+  },
+  {
+    "revision": "0237213294ec82fa9bfa",
+    "url": "/js/chunk-2d0cf2b2.068a9e3e.js"
+  },
+  {
+    "revision": "c443fff5981770168d9d",
+    "url": "/js/chunk-2d0cfa15.75dd0b02.js"
+  },
+  {
+    "revision": "525491714db7813e42fa",
+    "url": "/js/chunk-2d0d6376.74912b5c.js"
+  },
+  {
+    "revision": "8f21f915908ced754690",
+    "url": "/js/chunk-2d21b867.d4bb0564.js"
+  },
+  {
+    "revision": "bd589593938152bcafa2",
+    "url": "/js/chunk-2d22d746.474f3c1e.js"
+  },
+  {
+    "revision": "5bb8dd91305bc92680ba",
+    "url": "/js/chunk-3ae0d7fc.166d12c4.js"
+  },
+  {
+    "revision": "f7905217d79ff38d352a",
+    "url": "/js/chunk-868b1ebc.6457c8aa.js"
+  },
+  {
+    "revision": "f32e4c6ddbfdbbcafc7b",
+    "url": "/js/chunk-vendors.8d0edc84.js"
+  },
+  {
+    "revision": "663d722c1d6002d8fe24c022d0753a17",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
